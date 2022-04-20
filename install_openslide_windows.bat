f17554a798ba07655d1e4c4c97e42d98bd24abd6
martvanrijthoven <mart.vanrijthoven@gmail.com>
2022-04-20 10:10:55 +0200

    openslide test windows

--- install_openslide_windows.bat
+++ install_openslide_windows.bat
@@ -2,3 +2,4 @@ mkdir -p c:\\openslide
 cd c:\\openslide
 curl -LO "https://github.com/openslide/openslide-winbuild/releases/download/v20171122/openslide-win64-20171122.zip"
 7z x openslide-win64-20171122.zip
+dir
