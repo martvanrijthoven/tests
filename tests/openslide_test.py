4d6381342b914f2b390a8f90389c70daa5c1c045
martvanrijthoven <mart.vanrijthoven@gmail.com>
2022-04-20 09:43:31 +0200

    openslide test windows

--- tests/openslide_test.py
+++ tests/openslide_test.py
@@ -4,14 +4,12 @@ class TestOpenslide:
     def test_import_openslide(self):
         if os.name == 'nt':
             _dll_path = os.getenv('OPENSLIDE_PATH')
-            if _dll_path is not None:
-                if hasattr(os, 'add_dll_directory'):
-                    # Python >= 3.8
-                    with os.add_dll_directory(_dll_path):
-                        print('import openslide on windows')
-                        import openslide
-                else:
-                    raise ValueError('Python>=3.8 required')
+            print(_dll_path)
+            # Python >= 3.8
+            with os.add_dll_directory(_dll_path):
+                print('import openslide on windows')
+                import openslide
+
         else:
             print('import openslide on non-windows')
             import openslide
