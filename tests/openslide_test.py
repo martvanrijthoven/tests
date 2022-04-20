edaeb12b7584fc5a5427c741d1c31ec7749d4e46
martvanrijthoven <mart.vanrijthoven@gmail.com>
2022-04-20 10:55:59 +0200

    openslide test windows

--- tests/openslide_test.py
+++ tests/openslide_test.py
@@ -3,9 +3,9 @@ import os
 class TestOpenslide:
     def test_import_openslide(self):
         if os.name == 'nt':
-            with os.add_dll_directory(r"D:\openslide-win64-20171122\bin"):
-                print('import openslide on windows')
-                import openslide
+            # with os.add_dll_directory(r"D:\openslide-win64-20171122\bin"):
+            print('import openslide on windows')
+            import openslide
         else:
             print('import openslide on non-windows')
             import openslide
